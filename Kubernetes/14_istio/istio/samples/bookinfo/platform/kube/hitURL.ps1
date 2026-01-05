8346df9f4c4a133674c17cf3342f7343cf4422df
Charan <charan@nextops.in>
2026-01-05 21:10:52 +0530

    istio code

---
+++ Kubernetes/14_istio/istio/samples/bookinfo/platform/kube/hitURL.ps1
@@ -0,0 +1,17 @@
+﻿$URLs = @(
+  'http://34.132.236.27/productpage' #change the public ip as per your setup
+)
+
+$minSeconds = 1 * 1
+$maxSeconds = 2 * 1
+
+# Loop forever
+while($true){
+  # Send requests to all 3 urls
+  $URLs |ForEach-Object {
+    (Invoke-WebRequest -Uri $_ | Select Content).content
+  }
+
+  # Sleep for a random duration between 10 and 15 minutes
+  Start-Sleep -Seconds (Get-Random -Min $minSeconds -Max $maxSeconds)
+}
